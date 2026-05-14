3e9de4027f17d170a6dca0a1f23a929b163ece6a
Christian <el-chris1@users.noreply.github.com>
2026-05-14 11:45:27 -0400

    hcajhglsdjhgdf

--- lambo-data.js
+++ lambo-data.js
@@ -1,11 +1,15 @@
-var stateData = {
+var lamboData = {
     LE: {
-		name: 'Lamborghini Egoista',
 		abbr: 'LE',
 		speed: '200 mph',
 		year: '2013',
 		funFact: 'fun fact the Lamborghini Egoista stands for "selfish," and features a single-seat cockpit, a removable steering wheel for entry/exit, an estimated $117 million value as of 2026, and radar-absorbing body materials'
         
 			},
-			
-            	}
+			AJ: {
+                abbr: 'AJ',
+                speed: ' 217 mph',
+                year: '2012',
+                funFact: 'The Lamborghini Aventador J is a one-off, "road-legal" speedster produced in 2012, sold immediately for $2.8 million. It features no roof, no windshield, and no air conditioning, requiring drivers to wear helmets to withstand its over 186 mph top speed. '
+            },
+        }
--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let stateList = document.getElementById('lamborghini-list');
 
 stateList.onchange = function() {
     let selected = this.value
- document.getElementById("info-abbreviation").textContent = stateData[selected].abbr;
-document.getElementById("info-speed").textContent = stateData[selected].speed;
-document.getElementById("info-year").textContent = stateData[selected].year;
-document.getElementById("info-fun-fact").textContent = stateData[selected].funFact;
+ document.getElementById("info-abbreviation").textContent = lambo-Data[selected].abbr;
+document.getElementById("info-speed").textContent = lambo-Data[selected].speed;
+document.getElementById("info-year").textContent = lamboData[selected].year;
+document.getElementById("info-funfact").textContent = lamboData[selected].funFact;
 
 
 }
