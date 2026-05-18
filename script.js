a0cdfe8e0b7828f04f8e8acf93584ca4d05dbe88
Christian <el-chris1@users.noreply.github.com>
2026-05-18 09:28:52 -0400

    Refactor Lamborghini facts feature: update HTML structure, enhance data model, and comment out unused JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let stateList = document.getElementById('lamborghini-list');
+// let stateList = document.getElementById('lamborghini-list');
 
-stateList.onchange = function() {
-    let selected = this.value
- document.getElementById("info-abbreviation").textContent = lambo-Data[selected].abbr;
-document.getElementById("info-speed").textContent = lambo-Data[selected].speed;
-document.getElementById("info-year").textContent = lamboData[selected].year;
-document.getElementById("info-funfact").textContent = lamboData[selected].funFact;
+// stateList.onchange = function() {
+//     let selected = this.value
+//  document.getElementById("info-abbreviation").textContent = lambo-Data[selected].abbr;
+// document.getElementById("info-speed").textContent = lambo-Data[selected].speed;
+// document.getElementById("info-year").textContent = lamboData[selected].year;
+// document.getElementById("info-funfact").textContent = lamboData[selected].funFact;
 
 
-}
+// }
+
